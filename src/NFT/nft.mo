40b373fb1324e889671192b51945ee331b7c1263
Bob Karemera Shema <bobshema14@gmail.com>
2022-12-07 15:01:12 +0000

    Minting from FrontEnd

--- src/NFT/nft.mo
+++ src/NFT/nft.mo
@@ -1,6 +1,6 @@
 import Principal "mo:base/Principal";
 
-actor class NFT (name: Text, owner: Principal, content: [Nat8]) {
+actor class NFT (name: Text, owner: Principal, content: [Nat8]) = this {
     let itemName = name;
     let nftOwner = owner;
     let imageBytes = content;
@@ -15,5 +15,9 @@ actor class NFT (name: Text, owner: Principal, content: [Nat8]) {
 
     public query func getImage () : async [Nat8] {
         return content;
+    };
+
+    public query func getCanisterId () : async Principal {
+        return Principal.fromActor(this);
     }
 }
--- src/opend_assets/src/components/Minter.jsx
+++ src/opend_assets/src/components/Minter.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { opend } from "../../../declarations/opend";
+import { Principal } from "@dfinity/principal";
 
 function Minter() {
+  const {register, handleSubmit} = useForm();
+
+  async function onSubmit(data){
+    const name = data.name;
+    const imageByteData = [...new Uint8Array(await data.image[0].arrayBuffer())];
+
+    const newNftId = await opend.mint(imageByteData, name);
+    console.log(newNftId.toText());
+  }
+
   return (
     <div className="minter-container">
       <h3 className="makeStyles-title-99 Typography-h3 form-Typography-gutterBottom">
@@ -12,6 +25,7 @@ function Minter() {
       <form className="makeStyles-form-109" noValidate="" autoComplete="off">
         <div className="upload-container">
           <input
+            {...register("image", {required: true})}
             className="upload"
             type="file"
             accept="image/x-png,image/jpeg,image/gif,image/svg+xml,image/webp"
@@ -23,6 +37,7 @@ function Minter() {
         <div className="form-FormControl-root form-TextField-root form-FormControl-marginNormal form-FormControl-fullWidth">
           <div className="form-InputBase-root form-OutlinedInput-root form-InputBase-fullWidth form-InputBase-formControl">
             <input
+              {...register("name", {required: true})}
               placeholder="e.g. CryptoDunks"
               type="text"
               className="form-InputBase-input form-OutlinedInput-input"
@@ -31,7 +46,7 @@ function Minter() {
           </div>
         </div>
         <div className="form-ButtonBase-root form-Chip-root makeStyles-chipBlue-108 form-Chip-clickable">
-          <span className="form-Chip-label">Mint NFT</span>
+          <span onClick={handleSubmit(onSubmit)} className="form-Chip-label">Mint NFT</span>
         </div>
       </form>
     </div>
